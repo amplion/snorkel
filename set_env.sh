c941eb6c7b81c89bcc9468d72ca6a9b66254a863
godelstheory <corey@amplion.com>
2017-09-21 14:18:22 -0700

    Not necessary to add project path: previously loaded.

--- set_env.sh
+++ set_env.sh
@@ -3,8 +3,6 @@
 export SNORKELHOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Snorkel home directory: $SNORKELHOME"
 export PYTHONPATH="$PYTHONPATH:$SNORKELHOME:$SNORKELHOME/treedlib"
-# Add project path
-export PYTHONPATH="$PYTHONPATH:$HOME/bmb-tools"
 export PATH="$PATH:$SNORKELHOME:$SNORKELHOME/treedlib"
 
 echo "Environment variables set!"
